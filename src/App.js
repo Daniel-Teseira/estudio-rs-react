6db1121e6c10dbff4ce17ff9ae959cc6101a5e6f
Daniel Martin Teseira <teseirad@gestion.com>
2023-10-25 11:59:21 -0300

    se agrego pagina de contacto y redireccion en navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar/Navbaar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/home/Home';
 import Error from './pages/error404/Error404';
+import {Contact} from './pages/contact/Contact';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/home' element={ <Home /> } />
         <Route path='/error' element={ <Error /> } />
         <Route path='*' element={ <Error/>} />
+        <Route path='/contact' element={ <Contact/> }/>  
       </Routes>
 
       <Footer/>
